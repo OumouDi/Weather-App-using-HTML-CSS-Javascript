46ba3645acefa34f6c10881f09b7e109aa3ebaed
OumouDi <124316387+OumouDi@users.noreply.github.com>
2024-03-06 12:46:18 +0100

    fetch data

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const container = document.querySelector('.container');
-const search = document.querySelector('.search-box button');
-const weatherBox = document.querySelector('.weather-box');
-const weatherDetails = document.querySelector('.weather-details');
-const error404 = document.querySelector('.not-found');
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
+
+search.addEventListener("click", () => {
+  const APIKey = "95a352ca0344816ebb4ac72ca837bede";
+  const city = document.querySelector(".search-box input").value;
+
+  if (city === "") return;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.cod === "404") {
+        container.style.height = "400px";
+        weatherBox.style.display = "none";
+        weatherDetails.style.display = "none";
+        error404.style.display = "block";
+        error404.classList.add("fadeIn");
+        return;
+      }
+
+      error404.style.display = "none";
+      error404.classList.remove("fadeIn");
+    });
+});
